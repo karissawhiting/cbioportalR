d615786e2ba98134c2a2410328a57bca255b3504
karissawhiting <whitingk@mskcc.org>
2022-04-19 18:32:10 -0400

    add patient level functions

--- tests/testthat/test-by-patient.R
+++ tests/testthat/test-by-patient.R
@@ -1,32 +1,110 @@
 
-# Sample ID AND Study ID Endpoints -----------------------------------------------------------
+
+# Patient ID Endpoints -----------------------------------------------------------
 
 test_that("test endpoints - with patient ID", {
 
+  db_test <- "public"
+  set_cbioportal_db(db = db_test)
+  study_id = "msk_impact_2017"
+  patient_id = c("P-0000034", "P-0000036")
 
-  set_cbioportal_db("public")
+  endpoint_funs <- c(
+    get_samples_by_patient = get_samples_by_patient,
+    get_clinical_by_patient = get_clinical_by_patient)
 
-  expect_error(
-    get_samples_by_patient(patient_id = c("P-0000034", "P-0000036")),
-    NA)
+  res <- purrr::map(endpoint_funs,
+                    function(fn) rlang::exec(fn, study_id = study_id,
+                                             patient_id = patient_id))
 
-  expect_message(
-    get_samples_by_patient(patient_id = c("P-0000034", "P-0000036")),
-    "No*")
 
-  res <- get_samples_by_patient(patient_id = c("P-0000034", "P-0000036"))
-  unique(res$studyId) == "msk_impact_2017"
+
+  expect_equal(names(res), names(endpoint_funs))
+
+
+})
+
+test_that("test endpoints - with patient  ID, no study ID", {
+
+  db_test <- "public"
+  set_cbioportal_db(db = db_test)
+  study_id = NULL
+  patient_id = c("P-0000034", "P-0000036")
+
+  endpoint_funs <- c(
+    get_samples_by_patient = get_samples_by_patient,
+    get_clinical_by_patient = get_clinical_by_patient)
+
+  res <- purrr::map(endpoint_funs,
+                    function(fn) rlang::exec(fn, study_id = study_id,
+                                             patient_id = patient_id))
+
+  expect_equal(names(res), names(endpoint_funs))
+
+
 })
 
 
+test_that("test endpoints - no patient/ sample_study_pair", {
 
-test_that("test endpoints - error with n patient ID", {
+  db_test <- "public"
+  set_cbioportal_db(db = db_test)
+  study_id = NULL
+  patient_id = NULL
 
-  set_cbioportal_db("public")
+  expect_error(get_samples_by_patient(study_id = study_id,
+                                       patient_id = patient_id), "You*")
+  expect_error(get_clinical_by_patient(study_id = study_id,
+                                       patient_id = patient_id), "You*")
 
-  expect_error(
-    get_samples_by_patient(),
-    "You*"
-    )
 
 })
+
+test_that("test endpoints - sample_study_pair", {
+
+  db_test <- "public"
+  set_cbioportal_db(db = db_test)
+
+  ex <- tibble::tribble(
+    ~patient_id, ~study_id,
+    "P-0001453", "blca_nmibc_2017",
+    "P-0002166", "blca_nmibc_2017",
+    "P-0003238", "blca_nmibc_2017",
+    "P-0000004", "msk_impact_2017",
+    "P-0000023", "msk_impact_2017"
+  )
+
+  endpoint_funs <- c(
+    get_clinical_by_patient = get_clinical_by_patient)
+
+  res <- purrr::map(endpoint_funs,
+                    function(fn) rlang::exec(fn, patient_study_pairs = ex))
+
+  expect_message(res <- purrr::map(endpoint_funs,
+                                   function(fn) rlang::exec(fn, patient_study_pairs = ex)), "*")
+
+
+  expect_equal(names(res), names(endpoint_funs))
+
+  expect_message(res <- purrr::map(endpoint_funs,
+                    function(fn) rlang::exec(fn, patient_study_pairs = ex,
+                                             clinical_attribute = "SEX")), NA)
+
+
+  expect_equal(names(res), names(endpoint_funs))
+
+})
+
+test_that("test endpoints - sample_study_pair wrong format", {
+
+  db_test <- "public"
+  set_cbioportal_db(db = db_test)
+
+  ex <- c("P-0000023-T01-IM3", "P-0000023-T01-IM3")
+
+
+  expect_error(get_clinical_by_patient(sample_study_pairs = ex), "*")
+
+
+})
+
