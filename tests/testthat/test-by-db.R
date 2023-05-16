6c796aa2e88dd00cb9b49f46b076c5597069c357
karissawhiting <whitingk@mskcc.org>
2023-05-16 16:49:39 -0400

    add lookup_id function

--- tests/testthat/test-by-db.R
+++ tests/testthat/test-by-db.R
@@ -18,3 +18,22 @@ test_that("test endpoints- no parameters works without error", {
   expect_equal(names(res), names(endpoint_funs))
 
 })
+
+
+# Test lookup_id() -------------------------------------------------------
+test_that("test lookup_id- works without error", {
+  skip_on_cran()
+  skip_if(httr::http_error("www.cbioportal.org/api"))
+
+  db_test <- "public"
+  set_cbioportal_db(db = db_test)
+
+  lookup_id <- c("P-0001453-T01-IM3", "P-0000004-T01-IM3", "TCGA-OR-A5JA")
+
+  expect_no_error( x <- lookup_id(lookup_id = lookup_id,  base_url = 'www.cbioportal.org/api'))
+  expect_no_error( y <- lookup_id(lookup_id = c(lookup_id, "wrong"),
+                                  base_url = 'www.cbioportal.org/api'))
+
+  expect_equal(x, y)
+
+})
